79044f44157d264ad87d52bd372d4aa1164dbe0f
Malo Ranzetti <maloranzetti@gmail.com>
2020-02-19 15:35:30 +0100

    init

--- test/ch/epfl/rigel/math/ClosedIntervalTest.java
+++ test/ch/epfl/rigel/math/ClosedIntervalTest.java
@@ -9,7 +9,7 @@ class ClosedIntervalTest {
     @Test
     void containsNonTrivialTest() {
         ClosedInterval a  = ClosedInterval.symmetric(4);
-        Double b = 0.12;
+        double b = 0.12;
         assertTrue(a.contains(b));
     }
 
@@ -17,18 +17,18 @@ class ClosedIntervalTest {
     void symmetricThrowsException(){
         assertThrows(IllegalArgumentException.class, () -> {ClosedInterval.symmetric(-1);});
     }
-//sefhjnsdkfjnasdfkhjasfmnxydf ajsnraksrw
+
     @Test
     void clipTrivialTest() {
         ClosedInterval a  = ClosedInterval.of(0, 1);
-        Double b = 0.12;
+        double b = 0.12;
         assertEquals(a.clip(b), b, 1e-6);
     }
 
     @Test
     void clipTrivialTest2() {
         ClosedInterval a  = ClosedInterval.of(0, 1);
-        Double b = 4.6;
+        double b = 4.6;
         assertEquals(a.clip(b), 1, 1e-6);
     }
 
