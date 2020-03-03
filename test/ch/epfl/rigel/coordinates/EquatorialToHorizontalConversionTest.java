98631f96818684f3c38b971b4474d0b26f00a118
Malo Ranzetti <maloranzetti@gmail.com>
2020-03-03 14:21:49 +0100

    continuing tests and merging

---
+++ test/ch/epfl/rigel/coordinates/EquatorialToHorizontalConversionTest.java
@@ -0,0 +1,17 @@
+package ch.epfl.rigel.coordinates;
+
+import org.junit.jupiter.api.Test;
+
+import java.time.ZonedDateTime;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class EquatorialToHorizontalConversionTest {
+
+    @Test
+    void apply() {
+
+        //EquatorialToHorizontalConversion converter = new EquatorialToHorizontalConversion(ZonedDateTime.of())
+
+    }
+}
