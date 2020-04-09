b1631f524df07a5c435524c172487a069b9e0309
Malo Ranzetti <maloranzetti@gmail.com>
2020-04-09 20:02:33 +0200

    Added test for JavaFX11

---
+++ test/ch/epfl/rigel/JavaFXTest.java
@@ -0,0 +1,10 @@
+package ch.epfl.rigel;
+
+import java.awt.*;
+
+public class JavaFXTest {
+    public static void main(String[] args) {
+        Color c = Color.RED;
+        System.out.println(c.getRed());
+    }
+}
