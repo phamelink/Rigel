0d8d74bbfcf7452af9570a372ac20a92f954da63
Philip <philip.hamelink@gmail.com>
2020-05-08 13:11:45 +0200

    no message

--- src/ch/epfl/rigel/gui/Main.java
+++ src/ch/epfl/rigel/gui/Main.java
@@ -1,9 +1,20 @@
 package ch.epfl.rigel.gui;
 
+import ch.epfl.rigel.coordinates.GeographicCoordinates;
 import javafx.application.Application;
+import javafx.geometry.Orientation;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.Separator;
+import javafx.scene.control.TextField;
+import javafx.scene.control.TextFormatter;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
+import javafx.util.converter.NumberStringConverter;
+
+import java.security.SecurityPermission;
+import java.util.function.UnaryOperator;
 
 public class Main extends Application {
     public static void main(String[] args) { launch(args); }
@@ -31,7 +42,84 @@ public class Main extends Application {
         primaryStage.setTitle("Rigel");
         primaryStage.setMinHeight(600);
         primaryStage.setMinWidth(800);
+
+        HBox primaryBox = controlBar();
+
+
+        bp.setTop(primaryBox);
+
+        primaryStage.setScene(new Scene(bp));
+        primaryStage.show();
     }
 
-    private HBox controlBar
+    private HBox controlBar() {
+        HBox controlBar = new HBox();
+        Separator separator = new Separator(Orientation.VERTICAL);
+        HBox obsPosBox = obsPosBox();
+
+
+
+
+        controlBar.getChildren().addAll(obsPosBox, separator);
+        controlBar.setStyle("-fx-spacing: 4; -fx-padding: 4;");
+        return controlBar;
+    }
+
+    private HBox obsPosBox() {
+        HBox observationPosBox = new HBox();
+
+        Label lonLabel = new Label("Longitude (°) :");
+        Label latLabel = new Label("Latitude (°) :");
+
+        TextField textFieldLon = new TextField("6,57");
+        textFieldLon.setStyle("-fx-pref-width: 60; -fx-alignment: baseline-right;");
+        TextFormatter<Number> textFormatterLon = textFormatter("lon");
+        textFieldLon.setTextFormatter(textFormatterLon);
+
+        TextField textFieldLat = new TextField();
+        textFieldLat.setStyle("-fx-pref-width: 60; -fx-alignment: baseline-right;");
+        TextFormatter<Number> textFormatterLat = textFormatter("lat");
+        textFieldLat.setTextFormatter(textFormatterLat);
+        textFieldLat.setText("46,52");
+
+        //To obtain value: textFormatterLat.getValue()
+
+        observationPosBox.getChildren().addAll(lonLabel, textFieldLon, latLabel, textFieldLat);
+        observationPosBox.setStyle("-fx-spacing: inherit; -fx-alignment: baseline-left;");
+
+        return observationPosBox;
+
+    }
+
+    private TextFormatter<Number> textFormatter(String lonOrLat) {
+        NumberStringConverter stringConverter =
+                new NumberStringConverter("#0.00");
+
+        UnaryOperator<TextFormatter.Change> degFilter = (change -> {
+            try {
+                String newText =
+                        change.getControlNewText();
+                double newDeg =
+                        stringConverter.fromString(newText).doubleValue();
+
+                    return (GeographicCoordinates.isValidLonDeg(newDeg) && lonOrLat == "lon") ||
+                            (GeographicCoordinates.isValidLatDeg(newDeg) && lonOrLat == "lat")
+                                ? change
+                                : null;
+            } catch (Exception e) {
+                return null;
+            }
+        });
+        return new TextFormatter<>(stringConverter, 0, degFilter);
+    }
+
+    private HBox obsInstBox() {
+        HBox obsIntBox = new HBox();
+
+        Label dateLabel = new Label("Date :");
+
+        obsIntBox.setStyle("-fx-spacing: inherit; -fx-alignment: baseline-left;");
+
+        return obsIntBox;
+    }
 }
